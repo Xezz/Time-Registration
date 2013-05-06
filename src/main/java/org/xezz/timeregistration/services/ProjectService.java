659dc4281fc0b3a500f964a8e8c948b181566dd9
Xezz <koch@xezz.org>
2013-05-07 01:46:54 +0200

    FIX: Create ProjectService interface with all methods that should be required

--- src/main/java/org/xezz/timeregistration/services/ProjectService.java
+++ src/main/java/org/xezz/timeregistration/services/ProjectService.java
@@ -1,9 +1,63 @@
 package org.xezz.timeregistration.services;
 
+import org.xezz.timeregistration.model.Customer;
+import org.xezz.timeregistration.model.Project;
+import org.xezz.timeregistration.model.Timeframe;
+
+import java.util.List;
+
 /**
  * User: Xezz
  * Date: 29.04.13
  * Time: 14:37
  */
 public interface ProjectService {
+    /**
+     * Get all Projects
+     * @return List of all Projects
+     */
+    List<Project> getAll();
+
+    /**
+     * List of Projects by name
+     * @param name of the Project
+     * @return List of all Projects with that name
+     */
+    List<Project> getByName(String name);
+
+    /**
+     * List of Projects by Customer
+     * @param c Customer
+     * @return List of all Projects of this Customer
+     */
+    List<Project> getByCustomer(Customer c);
+
+    /**
+     * Project by Timeframe
+     * @param t Timeframe assigned to a Project
+     * @return Project that contains this Timeframe
+     */
+    Project getByTimeFrame(Timeframe t);
+
+    /**
+     * Project by identifier
+     * @param id Long the id
+     * @return Project with this id or null if not exist
+     */
+    Project getById(Long id);
+
+    /**
+     * Persist a Project
+     * @param p Project to persist
+     * @return persisted Project
+     */
+    Project addNewProject(Project p);
+
+    /**
+     * Updated existing Project
+     * @param p Project that has been updated
+     * @return Project that has been persisted
+     */
+    Project updateProject(Project p);
+
 }
