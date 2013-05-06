852f7af20b39b579d078c7a5ab70b3363b188392
Xezz <koch@xezz.org>
2013-05-06 19:47:34 +0200

    NEW: ProjectServiceImpl empty stub

---
+++ src/main/java/org/xezz/timeregistration/services/impl/ProjectServiceImpl.java
@@ -0,0 +1,11 @@
+package org.xezz.timeregistration.services.impl;
+
+import org.xezz.timeregistration.services.ProjectService;
+
+/**
+ * User: Xezz
+ * Date: 05.05.13
+ * Time: 11:14
+ */
+public class ProjectServiceImpl implements ProjectService {
+}
