ce9be9d1064893672e7a12eb67f3855a56410997
Xezz <koch@xezz.org>
2013-06-01 02:56:15 +0200

    UPDATE: Implemented form to create a Coworker

---
+++ src/main/webapp/static/app/view/coworker/Create.js
@@ -0,0 +1,36 @@
+Ext.define('TR.view.coworker.Create', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.coworkercreate',
+    title: 'Mitarbeiter - Erstellen',
+    layout: 'fit',
+    autoShow: true,
+
+    initComponents: function() {
+        this.items = [
+            {
+                xtype: 'form',
+                items: [
+                    xtype: 'textfield',
+                    name: 'firstName',
+                    fieldLabel: 'Vorname'
+                ], [
+                    xtype: 'textfield',
+                    name: 'lastName',
+                    fieldLabel: 'Nachname'
+                ]
+            }
+        ];
+        this.buttons = [
+            {
+                text: 'Speichern',
+                action: 'save'
+            }, {
+                text: 'Abbrechen',
+                scope: this,
+                handle: this.close
+            }
+        ];
+
+        this.callParent(arguments);
+    }
+});
