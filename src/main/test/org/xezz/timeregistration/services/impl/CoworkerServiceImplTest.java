2a69bdcbf864b4a342e5371c7061f7df69caf529
Xezz <xezzxezz@gmail.com>
2013-06-08 15:33:45 +0200

    NEW: CoworkerServiceImplTest.java

---
+++ src/main/test/org/xezz/timeregistration/services/impl/CoworkerServiceImplTest.java
@@ -0,0 +1,77 @@
+package org.xezz.timeregistration.services.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * User: bkoch
+ * Date: 08.06.13
+ * Time: 13:59
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({})
+public class CoworkerServiceImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkersAll() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkersByFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkersByLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkersByFirstAndLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkersByProject() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkerById() throws Exception {
+
+    }
+
+    @Test
+    public void testCoworkerByTimeFrame() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNewCoworker() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateCoworker() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteCoworker() throws Exception {
+
+    }
+}
