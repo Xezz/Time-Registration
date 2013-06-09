7831935042fbe8bb5aaa9c2cb76a35989309a9fd
Xezz <xezzxezz@gmail.com>
2013-06-09 17:03:45 +0200

    NEW: ProjectDAOTest

---
+++ src/test/java/org/xezz/timeregistration/dao/ProjectDAOTest.java
@@ -0,0 +1,97 @@
+package org.xezz.timeregistration.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: bkoch
+ * Date: 09.06.13
+ * Time: 17:03
+ */
+public class ProjectDAOTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProjectId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetProjectId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDescription() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCustomerId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCustomerId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCreationDate() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCreationDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLastUpdatedDate() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLastUpdatedDate() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+
+    @Test
+    public void testReceiveCustomer() throws Exception {
+
+    }
+}
