b2635ac3bcd3044c5f2739173625faf9ac4c7bf3
Xezz <koch@xezz.org>
2013-06-14 00:17:37 +0200

    UPDATE: Added first integration test

--- src/integration-test/java/org/xezz/timeregistration/service/impl/ITCoworkerServiceImplTest.java
+++ src/integration-test/java/org/xezz/timeregistration/service/impl/ITCoworkerServiceImplTest.java
@@ -1,12 +1,32 @@
 package org.xezz.timeregistration.service.impl;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.xezz.timeregistration.config.AppTestConfig;
+import org.xezz.timeregistration.config.DataTestConfig;
+import org.xezz.timeregistration.dao.CoworkerDAO;
+import org.xezz.timeregistration.services.CoworkerService;
 import org.xezz.timeregistration.services.impl.CoworkerServiceImpl;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * User: bkoch
@@ -14,9 +34,21 @@ import org.xezz.timeregistration.services.impl.CoworkerServiceImpl;
  * Time: 13:59
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@TestExecutionListeners({})
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {AppTestConfig.class, DataTestConfig.class, CoworkerServiceImpl.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@ActiveProfiles("integration-test")
+@Configurable
 public class ITCoworkerServiceImplTest {
-    private CoworkerServiceImpl coworkerService;
+
+    // Required by DBUnit
+    @Resource
+    DataSource dataSource;
+
+    @Autowired
+    private CoworkerService coworkerService;
 
     @Before
     public void setUp() throws Exception {
@@ -30,6 +62,8 @@ public class ITCoworkerServiceImplTest {
 
     @Test
     public void testCoworkersAll() throws Exception {
+        assertNotNull("Service has not been injected", coworkerService);
+        assertNotNull("service.getAll returned null", coworkerService.coworkersAll());
 
     }
 
@@ -65,7 +99,27 @@ public class ITCoworkerServiceImplTest {
 
     @Test
     public void testAddNewCoworker() throws Exception {
-
+        CoworkerDAO coworker = new CoworkerDAO();
+        final String firstName = "Bastian";
+        coworker.setFirstName(firstName);
+        final String lastName = "Koch";
+        coworker.setLastName(lastName);
+        coworkerService.addNewCoworker(coworker);
+        final Iterable<CoworkerDAO> daoList = coworkerService.coworkersAll();
+        int daoListSize = 0;
+        CoworkerDAO receivedDao = new CoworkerDAO();
+        for (CoworkerDAO cDao : daoList) {
+            if (daoListSize == 0) {
+                receivedDao = cDao;
+            }
+            daoListSize++;
+        }
+        assertThat(daoListSize, is(not(0)));
+        assertThat(coworker.getFirstName(), is(receivedDao.getFirstName()));
+        assertThat(coworker.getLastName(), is(receivedDao.getLastName()));
+        assertThat(receivedDao.getCreationDate(), is(not(nullValue())));
+        assertThat(receivedDao.getLastUpdatedDate(), is(not(nullValue())));
+        assertThat(receivedDao.getCreationDate(), is(receivedDao.getLastUpdatedDate()));
     }
 
     @Test
