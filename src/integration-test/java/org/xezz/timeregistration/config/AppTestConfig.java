a76c0126155c6712496e6a379b3aa5aa12af9a6d
Xezz <koch@xezz.org>
2013-06-14 00:05:25 +0200

    NEW: Configuration for integration tests

---
+++ src/integration-test/java/org/xezz/timeregistration/config/AppTestConfig.java
@@ -0,0 +1,26 @@
+package org.xezz.timeregistration.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * User: Xezz
+ * Date: 12.06.13
+ * Time: 23:18
+ * Spring based bootstrap configuration for the whole application
+ */
+@EnableTransactionManagement
+@ComponentScan({"org.xezz.timeregistration.services", "org.xezz.timeregistration.model", "org.xezz.timeregistration.dao", "org.xezz.timeregistration.repository"})
+@EnableJpaRepositories("org.xezz.timeregistration.repository")
+// Disabled webconfig for now, as it should get loaded from the context
+@Import({DataTestConfig.class})
+@Configuration
+@EnableSpringConfigured
+@Profile("integration-test")
+public class AppTestConfig {
+}
---
+++ src/integration-test/java/org/xezz/timeregistration/config/DataTestConfig.java
@@ -0,0 +1,85 @@
+package org.xezz.timeregistration.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.OpenJpaDialect;
+import org.springframework.orm.jpa.vendor.OpenJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * User: Xezz
+ * Date: 12.06.13
+ * Time: 23:16
+ * A spring configuration class to enable testing of database stuff!
+ */
+@Configuration
+@Profile("integration-test")
+public class DataTestConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem:paging");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws Exception {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setPackagesToScan("org.xezz.timeregistration.model.Coworker",
+                "org.xezz.timeregistration.model.Customer",
+                "org.xezz.timeregistration.model.Project",
+                "org.xezz.timeregistration.model.TimeSpan");
+        entityManagerFactory.setPersistenceUnitName("timeregistration");
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManagerFactory.setJpaDialect(new OpenJpaDialect());
+        final Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)");
+        jpaProperties.setProperty("openjpa.Log", "SQL=TRACE");
+        jpaProperties.setProperty("openjpa.InitializeEagerly", "true");
+        jpaProperties.setProperty("openjpa.RuntimeUnenhancedClasses", "supported");
+        entityManagerFactory.setJpaProperties(jpaProperties);
+        //entityManagerFactory.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        OpenJpaVendorAdapter jpaVendorAdapter = new OpenJpaVendorAdapter();
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        jpaVendorAdapter.setDatabase(Database.HSQL);
+
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() throws Exception {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setPersistenceUnitName("timeregistration");
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
