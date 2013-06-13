8b163566c5d1cadacaa3e2122a2fae6366f87699
Xezz <koch@xezz.org>
2013-06-14 00:17:09 +0200

    UPDATE: Removed unneeded LOGGER

--- src/main/java/org/xezz/timeregistration/dao/ProjectDAO.java
+++ src/main/java/org/xezz/timeregistration/dao/ProjectDAO.java
@@ -1,7 +1,5 @@
 package org.xezz.timeregistration.dao;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -27,7 +25,6 @@ public class ProjectDAO {
 
     @Autowired
     CustomerRepository customerRepository;
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectDAO.class);
 
     public ProjectDAO() {
     }
