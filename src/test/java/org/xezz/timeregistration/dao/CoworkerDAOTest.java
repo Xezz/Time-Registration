5d0204d1ae73f77dea014d116982a220502b441f
Xezz <koch@xezz.org>
2013-06-15 11:55:25 +0200

    FIX: Test checking the wrong object

--- src/test/java/org/xezz/timeregistration/dao/CoworkerDAOTest.java
+++ src/test/java/org/xezz/timeregistration/dao/CoworkerDAOTest.java
@@ -46,7 +46,7 @@ public class CoworkerDAOTest {
         final Long id = dao.getCoworkerId() + 15L;
         CoworkerDAO assertee = new CoworkerDAO();
         assertee.setCoworkerId(id);
-        assertEquals("Test ID equality", id, dao.getCoworkerId());
+        assertEquals("Test ID equality", id, assertee.getCoworkerId());
         assertNotEquals("Test inequality of given ID and preset ID", coworkerId, assertee.getCoworkerId());
     }
 
