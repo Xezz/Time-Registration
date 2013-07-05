11b69e3fe45c2e697a7c6ff2d7267bd343031933
Xezz <koch@xezz.org>
2013-07-05 17:27:08 +0200

    NEW: Added stub integration test for TimeSpanServiceImpl

---
+++ trex-integrationtests/src/test/java/org/xezz/timeregistration/service/impl/ITTimeSpanServiceImplTest.java
@@ -0,0 +1,41 @@
+package org.xezz.timeregistration.service.impl;
+
+import org.junit.Test;
+import org.xezz.timeregistration.AbstractBaseITConfiguration;
+
+/**
+ * User: Xezz
+ * Date: 05.07.13
+ * Time: 17:24
+ */
+public class ITTimeSpanServiceImplTest extends AbstractBaseITConfiguration {
+
+    @Test
+    public void testGetByCoworker() throws Exception {
+
+    }
+    @Test
+    public void testGetByProject() throws Exception {
+
+    }
+    @Test
+    public void testGetById() throws Exception {
+
+    }
+    @Test
+    public void testAddNew() throws Exception {
+
+    }
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+}
