eed180f391c098e5d94572845daf92f174761c02
Xezz <koch@xezz.org>
2013-07-05 18:48:17 +0200

    FIX: Null checks in TimeSpanServiceImpl.java

--- trex-backend/src/main/java/org/xezz/timeregistration/service/impl/TimeSpanServiceImpl.java
+++ trex-backend/src/main/java/org/xezz/timeregistration/service/impl/TimeSpanServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.xezz.timeregistration.dao.CoworkerDAO;
 import org.xezz.timeregistration.dao.ProjectDAO;
 import org.xezz.timeregistration.dao.TimeSpanDAO;
+import org.xezz.timeregistration.model.Coworker;
+import org.xezz.timeregistration.model.Project;
 import org.xezz.timeregistration.model.TimeSpan;
 import org.xezz.timeregistration.repository.CoworkerRepository;
 import org.xezz.timeregistration.repository.ProjectRepository;
@@ -34,47 +36,50 @@ public class TimeSpanServiceImpl implements TimeSpanService {
 
     @Override
     public Iterable<TimeSpanDAO> getByCoworker(CoworkerDAO coworkerDAO) {
-        final Iterable<TimeSpan> byCoworker = repo.findByCoworker(coworkerRepository.findOne(coworkerDAO.getCoworkerId()));
-        return getTimeSpanDAOs(byCoworker);
-    }
-
-    private List<TimeSpanDAO> getTimeSpanDAOs(Iterable<TimeSpan> timeSpanList) {
-        final List<TimeSpanDAO> daoList = new ArrayList<TimeSpanDAO>();
-        for (TimeSpan t : timeSpanList) {
-            daoList.add(new TimeSpanDAO(t));
+        Iterable<TimeSpan> byCoworker = new ArrayList<TimeSpan>();
+        if (coworkerDAO != null) {
+            final Coworker coworker = coworkerRepository.findOne(coworkerDAO.getCoworkerId());
+            if (coworker != null) {
+                byCoworker = repo.findByCoworker(coworker);
+            }
         }
-        return daoList;
+        return getTimeSpanDAOs(byCoworker);
     }
 
     @Override
     public Iterable<TimeSpanDAO> getByProject(ProjectDAO projectDAO) {
-        final Iterable<TimeSpan> byProject = repo.findByProject(projectRepository.findOne(projectDAO.getProjectId()));
+        Iterable<TimeSpan> byProject = new ArrayList<TimeSpan>();
+        if (projectDAO!= null) {
+            final Project project = projectRepository.findOne(projectDAO.getProjectId());
+            if (project != null) {
+                byProject = repo.findByProject(project);
+            }
+        }
         return getTimeSpanDAOs(byProject);
     }
 
-
     @Override
     public TimeSpanDAO getById(Long id) {
-        return new TimeSpanDAO(repo.findOne(id));
+        final TimeSpan entity = repo.findOne(id);
+        return entity != null ? new TimeSpanDAO(entity) : null;
     }
+
 
     @Transactional
     @Override
     public TimeSpanDAO addNew(TimeSpanDAO timeSpanDAO) {
-        return new TimeSpanDAO(repo.save(new TimeSpan(timeSpanDAO)));
+        return timeSpanDAO != null ? new TimeSpanDAO(repo.save(new TimeSpan(timeSpanDAO))) : null;
     }
 
-    // FIXME: Actually make the TimeSpan clone from TimeSpanDao?
     @Transactional
     @Override
     public TimeSpanDAO update(TimeSpanDAO timeSpanDAO) {
-        return new TimeSpanDAO(repo.save(new TimeSpan(timeSpanDAO)));
+        return timeSpanDAO != null ? new TimeSpanDAO(repo.save(new TimeSpan(timeSpanDAO))) : null;
     }
 
     @Override
     public Iterable<TimeSpanDAO> getAll() {
-        final Iterable<TimeSpan> all = repo.findAll();
-        return getTimeSpanDAOs(all);
+        return getTimeSpanDAOs(repo.findAll());
     }
 
     @Override
@@ -83,5 +88,19 @@ public class TimeSpanServiceImpl implements TimeSpanService {
         if (timeSpan != null) {
             repo.delete(timeSpan);
         }
+    }
+
+    /**
+     * Helper method to convert TimeSpan's to TimeSpanDAO's
+     *
+     * @param timeSpanList all TimeSpan's to convert
+     * @return resulting Iterable of TimeSpanDAO's
+     */
+    private Iterable<TimeSpanDAO> getTimeSpanDAOs(Iterable<TimeSpan> timeSpanList) {
+        final List<TimeSpanDAO> daoList = new ArrayList<TimeSpanDAO>();
+        for (TimeSpan t : timeSpanList) {
+            daoList.add(new TimeSpanDAO(t));
+        }
+        return daoList;
     }
 }
