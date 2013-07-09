6e9048975759e1b05247dbfbc432ca8f11548e33
Xezz <koch@xezz.org>
2013-07-09 11:35:06 +0200

    FIX: Removed recursive delete from build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf ~/servers/apache-tomee-webprofile/webapps/t-regs.war
-mvn clean install && cp trex-webapp/target/t-regs.war ~/servers/apache-tomee-webprofile/webapps/
-say Can not hear you through the SOUND of how AWESOME I R
+rm ~/servers/apache-tomee-webprofile/webapps/t-regs.war
+mvn clean install && cp trex-webapp/target/t-regs.war ~/servers/apache-tomee-webprofile/webapps/ && say Compilation succeeded and deployment finished!
 
