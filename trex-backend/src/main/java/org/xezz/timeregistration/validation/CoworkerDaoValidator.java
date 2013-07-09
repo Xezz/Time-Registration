6311cc4c4a4c1ad6dacb7f63ee09ee3c431fecee
Xezz <koch@xezz.org>
2013-07-09 10:55:22 +0200

    UPDATE: CoworkerDAOValidator corrected property binding names

--- trex-backend/src/main/java/org/xezz/timeregistration/validation/CoworkerDaoValidator.java
+++ trex-backend/src/main/java/org/xezz/timeregistration/validation/CoworkerDaoValidator.java
@@ -1,6 +1,7 @@
 package org.xezz.timeregistration.validation;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 import org.xezz.timeregistration.dao.CoworkerDAO;
 
@@ -19,16 +20,12 @@ public class CoworkerDAOValidator implements Validator {
     @Override
     public void validate(Object o, Errors errors) {
         final CoworkerDAO coworker = (CoworkerDAO) o;
-        if (coworker.getFirstName() == null || coworker.getFirstName().trim().equals("")) {
-            errors.rejectValue("firstName", "First name empty");
-        }
-        if (coworker.getLastName() == null || coworker.getLastName().trim().equals("")) {
-            errors.rejectValue("lastName", "Last name empty");
-        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "field.required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "field.required");
         // If an id, creation date or last updated date exists all others have to exist too
         if ((coworker.getCoworkerId() != null || coworker.getCreationDate() != null || coworker.getLastUpdatedDate() != null) &&
                 (coworker.getCoworkerId() == null || coworker.getCreationDate() == null || coworker.getLastUpdatedDate() == null)) {
-            errors.rejectValue("Meta data", "Incomplete meta data");
+            errors.reject("Meta data", "Incomplete meta data");
         }
     }
 }
